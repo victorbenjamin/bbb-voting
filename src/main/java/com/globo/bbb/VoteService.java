6e0c73f2d4e4aebafa64809a8bf357ca6a5fe0a9
Victor Benjamin <victor.benjamin@gmail.com>
2017-11-15 11:58:29 -0200

    primeiros testes ja com backpressure

--- src/main/java/com/globo/bbb/VoteService.java
+++ src/main/java/com/globo/bbb/VoteService.java
@@ -1,14 +1,44 @@
 package com.globo.bbb;
 
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+import java.util.concurrent.TimeUnit;
+
 public class VoteService {
 
+    private Subject<Boolean> participant1;
+    private Subject<Boolean> participant2;
+    private Long count1 = 0L;
+    private Long count2 = 0L;
+
+    public VoteService() {
+        this(Schedulers.computation());
+    }
+
+    public VoteService(Scheduler scheduler) {
+        this.participant1 = PublishSubject.create();
+        this.participant1.buffer(1, TimeUnit.SECONDS, scheduler).subscribe(c -> this.count1 += c.size());
+        this.participant2 = PublishSubject.create();
+        this.participant2.buffer(1, TimeUnit.SECONDS, scheduler).subscribe(c -> this.count2 += c.size());
+    }
 
     public void voteParcticipant1() {
-
+        participant1.onNext(true);
     }
 
     public void voteParcticipant2() {
+        participant2.onNext(true);
+    }
 
+    public long getVotesParticipant1() {
+        return this.count1;
+    }
+
+    public long getVotesParticipant2() {
+        return this.count2;
     }
 
 }
--- src/test/java/com/globo/bbb/BbbApplicationTests.java
+++ src/test/java/com/globo/bbb/BbbApplicationTests.java
@@ -1,18 +1,36 @@
 package com.globo.bbb;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.TestScheduler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BbbApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		Assert.assertTrue(true);
+	public void testZeroVotes() {
+		VoteService service = new VoteService();
+		assertEquals(0, service.getVotesParticipant1());
+        assertEquals(0, service.getVotesParticipant2());
 	}
+
+    @Test
+    public void testOneVotes() {
+        TestScheduler scheduler = new TestScheduler();
+        VoteService service = new VoteService(scheduler);
+        service.voteParcticipant1();
+        service.voteParcticipant2();
+        scheduler.advanceTimeBy(1200, TimeUnit.MILLISECONDS);
+        assertEquals(1, service.getVotesParticipant1());
+        assertEquals(1, service.getVotesParticipant2());
+    }
 
 }
