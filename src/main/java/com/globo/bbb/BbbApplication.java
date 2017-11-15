8760800009df1eca361bf1eb328ceb8f6c640721
Victor Benjamin <victor.benjamin@gmail.com>
2017-11-15 13:23:16 -0200

    tirando lixo.

--- src/main/java/com/globo/bbb/BbbApplication.java
+++ src/main/java/com/globo/bbb/BbbApplication.java
@@ -1,7 +1,5 @@
 package com.globo.bbb;
 
-import io.reactivex.Observable;
-import io.reactivex.subjects.PublishSubject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,14 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BbbApplication {
 
 	public static void main(String[] args) {
-
-		PublishSubject<Integer> subject = PublishSubject.create();
-		subject.onNext(1);
-		subject.subscribe(System.out::println);
-		subject.onNext(2);
-		subject.onNext(3);
-		subject.onNext(4);
-//		SpringApplication.run(BbbApplication.class, args);
+		SpringApplication.run(BbbApplication.class, args);
 
 	}
 }
